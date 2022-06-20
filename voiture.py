d29aa04e48e16d3648fd4ae9796b4a035e176f1c
ludo2ne <92184287+ludo2ne@users.noreply.github.com>
2022-06-20 15:48:01 +0200

    Update voiture.py

--- voiture.py
+++ voiture.py
@@ -8,4 +8,4 @@ class Voiture:
     def accelere(self, increment):
         if increment > 10:
             increment = 10
-        self.vitesse = min(130, self.vitesse + increment)
+        self.vitesse = min(110, self.vitesse + increment)
